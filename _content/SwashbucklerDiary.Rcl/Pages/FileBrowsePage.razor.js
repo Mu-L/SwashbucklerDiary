78cda4237b0d3775caf48554995786b996c1e46a
Yu-Core <Yu-Core@users.noreply.github.com>
2024-05-25 08:14:54 +0000

    deploy: 1a5b821036020ade6209fe2981fa297287f77e4a

---
+++ _content/SwashbucklerDiary.Rcl/Pages/FileBrowsePage.razor.js
@@ -0,0 +1,61 @@
+﻿export function recordScrollInfo(selectors) {
+    selectors.forEach(s => {
+        const element = document.querySelector(s);
+        if (element) {
+            scrollListener(element, () => {
+                if (element.disallowRecordScrollInfo) {
+                    return;
+                }
+
+                element.previousScrollInfo = {
+                    scrollHeight: element.scrollHeight,
+                    scrollTop: element.scrollTop
+                };
+            });
+        }
+    });
+}
+
+export function stopRecordScrollInfo(selectors) {
+    selectors.forEach(s => {
+        const element = document.querySelector(s);
+        if (element) {
+            element.disallowRecordScrollInfo = true;
+        }
+    });
+}
+
+function scrollListener(element, doSomething) {
+    let ticking = false;
+
+    element.addEventListener("scroll", (event) => {
+        if (!ticking) {
+            window.requestAnimationFrame(() => {
+                doSomething();
+                ticking = false;
+            });
+
+            ticking = true;
+        }
+    });
+}
+
+export function restoreScrollPosition(selectors) {
+    const anchorScroll = (ele, scrollInfo) => {
+        const scrollTop = scrollInfo.scrollTop + ele.scrollHeight - scrollInfo.scrollHeight;
+        ele.scrollTo({
+            top: scrollTop,
+            left: 0,
+            behavior: "auto",
+        });
+
+        ele.disallowRecordScrollInfo = false;
+    };
+
+    selectors.forEach(s => {
+        const element = document.querySelector(s);
+        if (element && element.previousScrollInfo) {
+            anchorScroll(element, element.previousScrollInfo);
+        }
+    });
+}
---
+++ _content/SwashbucklerDiary.Rcl/js/utility.js
@@ -0,0 +1,18 @@
+﻿function preventDefaultOnmousedown(el) {
+    if (el == null) {
+        return;
+    }
+
+    el.onmousedown = (e) => {
+        e.preventDefault();
+    }
+}
+
+function setLanguage(lang) {
+    document.documentElement.lang = lang;
+}
+
+function addToHead(html) {
+    const element = document.createRange().createContextualFragment(html);
+    document.getElementsByTagName('head')[0].appendChild(element);
+}
